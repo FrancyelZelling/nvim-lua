39e7284544fbffe90a99cd018a7f60e9318b49bf
FrancyelZelling <zellingf@gmail.com>
2021-11-11 06:16:25 -0300

    Added clipboard support for wsl

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -56,3 +56,15 @@ cnoreabbrev w w!
 cnoreabbrev Q q
 cnoreabbrev Qall qall
 ]]
+
+-- WSL yank
+vim.cmd[[
+" WSL yank support
+let s:clip = '/mnt/c/Windows/System32/clip.exe'  " change this path according to your mount point
+if executable(s:clip)
+    augroup WSLYank
+        autocmd!
+        autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
+    augroup END
+endif
+]]
